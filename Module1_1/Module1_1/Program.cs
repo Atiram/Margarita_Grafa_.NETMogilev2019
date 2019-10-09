3a5c1ab5d0ccdc1ca097f617f76db41dd404176e
Margarita Grafa <atiram2709@gmail.com>
2019-10-09 22:07:33 +0300

    New project was created. Task 1 was completed.

---
+++ Module1_1/Module1_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input data
+            bool check = true;
+            int firstPoint = 0;
+            int secondPoint = 0;
+            while (check)
+            {
+                Console.WriteLine("Input integer firstPoint");
+                if (int.TryParse(Console.ReadLine(), out firstPoint))
+                    check = false;
+                else
+                    Console.WriteLine("Incorrect data.Try again.");
+            }
+
+            check = true;
+            while (check)
+            {
+                Console.WriteLine("Input integer secondPoint");
+                if (int.TryParse(Console.ReadLine(), out secondPoint))
+                    check = false;
+                else
+                    Console.WriteLine("Incorrect data.Try again.");
+            }
+
+            //Swap
+            firstPoint += secondPoint;
+            secondPoint = firstPoint - secondPoint;
+            firstPoint -= secondPoint;
+
+            //Output data
+            Console.WriteLine("firstPoint=" + firstPoint);
+            Console.WriteLine("secondPoint=" + secondPoint);
+            Console.ReadKey();
+        }
+    }
+}
