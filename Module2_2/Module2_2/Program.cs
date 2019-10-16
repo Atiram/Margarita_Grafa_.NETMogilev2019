012bc46cd516aacae4f0a57a6081250b79a7c8a3
Margarita Grafa <atiram2709@gmail.com>
2019-10-17 00:03:41 +0300

    Module 2 Task 1

---
+++ Module2_2/Module2_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Module2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input data.
+            uint number = 0;
+            Console.WriteLine("Input the number");
+            while (!(uint.TryParse(Console.ReadLine(), out number)))
+                Console.WriteLine("Incorrect data.Try again.");
+
+            // Output data.
+            if ((number % 2 == 0) && (number >= 18))
+                Console.WriteLine("Happy eighteenth birthday!");
+            if ((number % 2 != 0) && (number < 18) && (number > 13))
+                Console.WriteLine("Congratulations on your transfer to high school!");
+            Console.ReadKey();
+        }
+    }
+}
