d06d3a0861c0e9a9cc66ea0cfdadd8969a17f939
Margarita Grafa <atiram2709@gmail.com>
2019-10-16 22:50:58 +0300

    Module 2 Task 1 was completed.

---
+++ Module2_1/Module2_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Module2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input data.
+            Console.WriteLine("Input the number of companies");
+            uint numberOfCompanies = Input();
+            Console.WriteLine("Input tax using persents");
+            double tax = InputPercent();
+            Console.WriteLine("Input the profit of companies");
+            uint profitOfCompanies = Input();
+
+            // Calculation.
+            double summaryTax = profitOfCompanies * numberOfCompanies * tax / 100;
+
+            // Output data.
+            Console.WriteLine("Summary tax: " + summaryTax);
+            Console.ReadKey();
+        }
+        public static uint Input()
+        {
+            uint inputValue;
+            while (!(uint.TryParse(Console.ReadLine(), out inputValue)))
+                Console.WriteLine("Incorrect data. Try again.");
+            return inputValue;
+        }
+
+        public static double InputPercent()
+        {
+            double inputValue;
+            while (!(double.TryParse(Console.ReadLine().Replace(".", ","), out inputValue)) || (inputValue < 0 || inputValue > 100))
+                Console.WriteLine("Incorrect data. Try again.");
+            return inputValue;
+        }
+    }
+}
