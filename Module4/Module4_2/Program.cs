a668f16322a8662060e97250099a85c4dbfbb9ed
Margarita Grafa <atiram2709@gmail.com>
2019-10-31 23:29:31 +0300

    Module4 was done

---
+++ Module4/Module4_2/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+namespace Module4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the number of required operation. \n1 - add three int numbers \n2 - add two int numbers \n3 - add three double numbers \n4 - concatination two strings \n5 - add elements of two arrays");
+            uint numberOperation;
+            InputData(out numberOperation);
+            switch (numberOperation)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Input three integer numbers");
+                        InputData(out int firstPoint);
+                        InputData(out int secondPoint);
+                        InputData(out int thirdPoint);
+                        Console.WriteLine($"Result = {AddValues(firstPoint, secondPoint, thirdPoint)}");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Input two integer numbers");
+                        InputData(out int firstPoint);
+                        InputData(out int secondPoint);
+                        Console.WriteLine($"Result = {AddValues(firstPoint, secondPoint)}");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Input three double numbers");
+                        InputData(out double firstPoint);
+                        InputData(out double secondPoint);
+                        InputData(out double thirdPoint);
+                        Console.WriteLine($"Result = {AddValues(firstPoint, secondPoint, thirdPoint)}");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Input two strings");
+                        string firstString = Console.ReadLine();
+                        string secondString = Console.ReadLine();
+                        Console.WriteLine($"Result = {AddValues(firstString, secondString)}");
+                        break;
+                    }
+                case 5:
+                    {
+                        InputData(out int[] firstArray);
+                        InputData(out int[] secondArray);
+                        Console.WriteLine("Addition of two arrays: ");
+                        DisplayArray(AddValues(firstArray, secondArray));
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Incorrect number of operation.");
+                        break;
+                    }
+            }
+        }
+
+        // Verification of the integer number.
+        public static bool IsValidData(out int inputNumber)
+        {
+            return (int.TryParse(Console.ReadLine(), out inputNumber));
+        }
+
+        // Verification of the unsigned integer number.
+        public static bool IsValidData(out uint inputNumber)
+        {
+            return (uint.TryParse(Console.ReadLine(), out inputNumber));
+        }
+
+        // Verification of the double number with "," or ".".
+        public static bool IsValidData(out double inputNumber)
+        {
+            return (double.TryParse(Console.ReadLine().Replace(".", ","), out inputNumber));
+        }
+
+        // Input the integer number.
+        public static void InputData(out int newNumber)
+        {
+            while (!IsValidData(out newNumber))
+            {
+                Console.WriteLine("Incorrect data. Try again using interger number.");
+            }
+        }
+
+        // Input the unsigned integer number.
+        public static void InputData(out uint newNumber)
+        {
+            while (!IsValidData(out newNumber))
+            {
+                Console.WriteLine("Incorrect data. Try again using interger number.");
+            }
+        }
+
+        // Input double number.
+        public static void InputData(out double newNumber)
+        {
+            while (!IsValidData(out newNumber))
+            {
+                Console.WriteLine("Incorrect data. Try again using double number.");
+            }
+        }
+
+        // Creation of integer array.
+        public static void InputData(out int[] workingArray)
+        {
+            Console.WriteLine("Input the length of the array");
+            uint arrayLength;
+            InputData(out arrayLength);
+            Console.WriteLine($"Input {arrayLength} elements of the array");
+            workingArray = new int[arrayLength];
+            for (int i = 0; i < workingArray.Length; i++)
+            {
+                InputData(out workingArray[i]);
+            }
+        }
+
+        // Add two or three integer numbers.
+        public static int AddValues(int firstNumber, int secondNumber, int thirdNumber = 0)
+        {
+            return firstNumber + secondNumber + thirdNumber;
+        }
+
+        // Add three double numbers.
+        public static double AddValues(double firstNumber, double secondNumber, double thirdNumber)
+        {
+            return firstNumber + secondNumber + thirdNumber;
+        }
+
+        // Concatenation of two stringgs.
+        public static string AddValues(string firstString, string secondString)
+        {
+            return String.Concat(firstString, secondString);
+        }
+
+        // Add of the elements of two arrays.
+        public static int[] AddValues(int[] firstArray, int[] secondArray)
+        {
+            if (firstArray.Length >= secondArray.Length)
+            {
+                for (int i = 0; i < secondArray.Length; i++)
+                {
+                    firstArray[i] += secondArray[i];
+                }
+                return firstArray;
+            }
+            else
+            {
+                for (int i = 0; i < firstArray.Length; i++)
+                {
+                    secondArray[i] += firstArray[i];
+                }
+                return secondArray;
+            }
+        }
+
+        // Output of the array.
+        public static void DisplayArray(int[] workingArray)
+        {
+            Console.WriteLine(String.Join(" ", workingArray));
+        }
+    }
+}
---
+++ Module4/Module4_4/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Module4_4
+{
+    class Program
+    {
+        const int INCREASE_NUMBER = 10;
+        static void Main(string[] args)
+        {
+            // Input and increasing 3 numbers.
+            Console.WriteLine("Input 3 numbers");
+            (int, int, int) tupleOfNumbers;
+            InputData(out tupleOfNumbers);
+            IncreaseOfTen(ref tupleOfNumbers);
+            Console.WriteLine($"Numbers after increasing of 10: {tupleOfNumbers.Item1}, {tupleOfNumbers.Item2}, {tupleOfNumbers.Item3}");
+
+            // Input radius, calculation and output of the circumference and the area of circle.
+            Console.WriteLine("Input radius");
+            int radius;
+            InputData(out radius);
+            (double, double) tupleCirlceParametrs;
+            CalculateCirceParametrs(radius, out tupleCirlceParametrs);
+            Console.WriteLine($"Circumference of the circle is {tupleCirlceParametrs.Item1}");
+            Console.WriteLine($"Area of the circle is {tupleCirlceParametrs.Item2}");
+
+            // Intut the length and the array of this length.
+            Console.WriteLine("Input the length of the array");
+            uint arrayLength;
+            InputData(out arrayLength);
+            Console.WriteLine($"Input {arrayLength} elements of the array");
+            int[] workingArray = new int[arrayLength];
+            for (int i = 0; i < workingArray.Length; i++)
+            {
+                InputData(out workingArray[i]);
+            }
+
+            // Search of the minElement and maxElement, calculate sum of the elements.
+            (int, int, int) tupleArrayValues;
+            CalculateArrayValues(workingArray, out tupleArrayValues);
+            Console.WriteLine($"Minimum is {tupleArrayValues.Item1}");
+            Console.WriteLine($"Maximum is {tupleArrayValues.Item2}");
+            Console.WriteLine($"Sum of the elements is {tupleArrayValues.Item3}");
+        }
+
+        // Verification of the unsigned integer number.
+        public static bool IsValidData(out uint inputNumber)
+        {
+            return (uint.TryParse(Console.ReadLine(), out inputNumber));
+        }
+
+        // Verification of the integer number.
+        public static bool IsValidData(out int inputNumber)
+        {
+            return (int.TryParse(Console.ReadLine(), out inputNumber));
+        }
+
+        // Input the unsigned integer.
+        public static void InputData(out uint newNumber)
+        {
+            while (!IsValidData(out newNumber))
+            {
+                Console.WriteLine("Incorrect data. Try again using unsigned interger number.");
+            }
+        }
+
+        // Input integer number.
+        public static void InputData(out int newNumber)
+        {
+            while (!IsValidData(out newNumber))
+            {
+                Console.WriteLine("Incorrect data. Try again.");
+            }
+        }
+
+        // Input tuple number.
+        public static void InputData(out (int, int, int) newNumber)
+        {
+            while (!IsValidData(out newNumber.Item1))
+            {
+                Console.WriteLine("Incorrect data. Try again.");
+            }
+            while (!IsValidData(out newNumber.Item2))
+            {
+                Console.WriteLine("Incorrect data. Try again.");
+            }
+            while (!IsValidData(out newNumber.Item3))
+            {
+                Console.WriteLine("Incorrect data. Try again.");
+            }
+        }
+
+        // Increasing the number of 10.
+        public static void IncreaseOfTen(ref (int, int, int) workingTuple)
+        {
+            workingTuple.Item1 += INCREASE_NUMBER;
+            workingTuple.Item2 += INCREASE_NUMBER;
+            workingTuple.Item3 += INCREASE_NUMBER;
+        }
+
+        //Calculation of circumference and area of the circle.
+        public static void CalculateCirceParametrs(int radius, out (double, double) workingTuple)
+        {
+            workingTuple.Item1 = 2 * radius * Math.PI;
+            workingTuple.Item2 = radius * radius * Math.PI;
+        }
+
+        // Calculation of min element, max element and sum of the elemetns.
+        public static void CalculateArrayValues(int[] array, out (int, int, int) workingTuple)
+        {
+            int min = array[0];
+            int max = array[0];
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+                sum += array[i];
+            }
+            workingTuple.Item1 = min;
+            workingTuple.Item2 = max;
+            workingTuple.Item3 = sum;
+        }
+    }
+}
